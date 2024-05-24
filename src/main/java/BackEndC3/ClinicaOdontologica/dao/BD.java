ac4cb552f4d05a3423092f6222beae33c5912489
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-05-23 21:48:05 -0300

    Arreglo SQL

--- src/main/java/BackEndC3/ClinicaOdontologica/dao/BD.java
+++ src/main/java/BackEndC3/ClinicaOdontologica/dao/BD.java
@@ -11,9 +11,17 @@ import java.time.LocalDate;
 public class BD {
 
     private static final Logger logger= Logger.getLogger(BD.class);
-    private static final String SQL_DROP_CREATE_PAC="DROP TABLE IF EXISTS PACIENTES; CREATE TABLE PACIENTES (" +
-            "ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(100) NOT NULL, APELLIDO VARCHAR(100) NOT NULL, " +
-            "CEDULA VARCHAR(100) NOT NULL, FECHA_INGRESO DATE NOT NULL, DOMICILIO_ID INT NOT NULL, EMAIL VARCHAR(100) NOT NULL, ODONTOLOGO_ID INT NOT NULL)"; //<<-- FK
+    private static final String SQL_DROP_CREATE_PAC =
+            "DROP TABLE IF EXISTS PACIENTES; " +
+                    "CREATE TABLE PACIENTES (" +
+                    "ID INT AUTO_INCREMENT PRIMARY KEY, " +
+                    "NOMBRE VARCHAR(100) NOT NULL, " +
+                    "APELLIDO VARCHAR(100) NOT NULL, " +
+                    "CEDULA VARCHAR(100) NOT NULL, " +
+                    "FECHA_INGRESO DATE NOT NULL, " +
+                    "DOMICILIO_ID INT NOT NULL, " +
+                    "EMAIL VARCHAR(100) NOT NULL, " +
+                    "ODONTOLOGO_ID INT NOT NULL);"; //<<-- FK
 
     private static final String SQL_DROP_CREATE_DOM="DROP TABLE IF EXISTS DOMICILIOS; " +
             "CREATE TABLE DOMICILIOS (ID INT AUTO_INCREMENT PRIMARY KEY, CALLE VARCHAR(100) NOT NULL, NUMERO INT NOT NULL, LOCALIDAD VARCHAR(100) NOT NULL, PROVINCIA VARCHAR(100) NOT NULL)";
@@ -23,8 +31,12 @@ public class BD {
 
     //datos de prueba
     private static final String SQL_PRUEBA_ODON="INSERT INTO ODONTOLOGOS (NOMBRE, APELLIDO, MATRICULA) VALUES ('Dr.Juan','Bonin', 1234), ('Dra.Joselin','Rosas', 5678);";
-    private static final String SQL_PRUEBA="INSERT INTO PACIENTES (NOMBRE, APELLIDO, CEDULA, FECHA_INGRESO, DOMICILIO_ID, EMAIL) VALUES ('Jorgito','Pereyra','111111','2024-05-16', 1,'jorge.pereyra@digitalhouse.com',1), ('German','Fraire','22222','2024-05-10',2,'german@german.com',2); " +
-            "INSERT INTO DOMICILIOS  (CALLE, NUMERO, LOCALIDAD, PROVINCIA) VALUES ('Siempre Viva',742,'Springfield','USA'),('Av. Uruguay',345,'Punta del Este','Uruguay')";
+    private static final String SQL_PRUEBA =
+            "INSERT INTO PACIENTES (NOMBRE, APELLIDO, CEDULA, FECHA_INGRESO, DOMICILIO_ID, EMAIL, ODONTOLOGO_ID) " +
+                    "VALUES ('Jorgito', 'Pereyra', '111111', '2024-05-16', 1, 'jorge.pereyra@digitalhouse.com', 1), " +
+                    "('German', 'Fraire', '22222', '2024-05-10', 2, 'german@german.com', 2); " +
+                    "INSERT INTO DOMICILIOS (CALLE, NUMERO, LOCALIDAD, PROVINCIA) " +
+                    "VALUES ('Siempre Viva', 742, 'Springfield', 'USA'), ('Av. Uruguay', 345, 'Punta del Este', 'Uruguay');";
 
 
     public static void crearTablas(){
