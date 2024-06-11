9ee6ef60f5dea6872c223e99879ca6e7e714ed15
dan-ruiz <danielruiz2009@hotmail.com>
2024-06-11 16:55:35 -0500

    Se corrige asociacion OneToMany y se agregan atributos Set<>

--- src/main/java/BackEndC3/ClinicaOdontologica/entity/Odontologo.java
+++ src/main/java/BackEndC3/ClinicaOdontologica/entity/Odontologo.java
@@ -1,8 +1,12 @@
 package BackEndC3.ClinicaOdontologica.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -13,24 +17,31 @@ public class Odontologo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private Integer matricula;
+
     @Column
     private String nombre;
+
     @Column
     private String apellido;
 
-    public Odontologo(Long id, Integer matricula, String nombre, String apellido) {
+    @OneToMany(mappedBy = "odontologo", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Paciente> pacientes = new HashSet<>();
+
+    @OneToMany(mappedBy = "odontologo", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Turno> turnos = new HashSet<>();
+
+    public Odontologo(Long id, Integer matricula, String nombre, String apellido, Set<Paciente> pacientes, Set<Turno> turnos) {
         this.id = id;
         this.matricula = matricula;
         this.nombre = nombre;
         this.apellido = apellido;
-
-    }
-    public Odontologo(Integer matricula, String nombre, String apellido) {
-        this.matricula = matricula;
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this.pacientes = pacientes;
+        this.turnos = turnos;
     }
 
     public Odontologo() {
--- src/main/java/BackEndC3/ClinicaOdontologica/entity/Paciente.java
+++ src/main/java/BackEndC3/ClinicaOdontologica/entity/Paciente.java
@@ -1,6 +1,7 @@
 package BackEndC3.ClinicaOdontologica.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -8,6 +9,8 @@ import lombok.Setter;
 import org.hibernate.engine.internal.Cascade;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,44 +20,44 @@ public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String nombre;
+
     @Column
     private String apellido;
+
     @Column
     private String cedula;
+
     @Column
     private LocalDate fechaIngreso;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "domicilio_id", referencedColumnName = "id")
     private Domicilio domicilio;
+
     @Column(unique = true, nullable = false)
     private String email;
+
     @ManyToOne
     @JoinColumn(name = "odontologo_id", referencedColumnName = "id")
     private Odontologo odontologo;
 
-    public Paciente(String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio, String email, Odontologo odontologo) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.cedula = cedula;
-        this.fechaIngreso = fechaIngreso;
-        this.domicilio = domicilio;
-        this.email= email;
-        this.odontologo= odontologo;
-    }
+    @OneToMany(mappedBy = "paciente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Turno> turnos = new HashSet<>();
 
-    public Paciente(Long id, String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio,
-                    String email, Odontologo odontologo) {
+    public Paciente(Long id, String nombre, String apellido, String cedula, LocalDate fechaIngreso, Domicilio domicilio, String email, Odontologo odontologo, Set<Turno> turnos) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.cedula = cedula;
         this.fechaIngreso = fechaIngreso;
         this.domicilio = domicilio;
-        this.email= email;
-        this.odontologo= odontologo;
-
+        this.email = email;
+        this.odontologo = odontologo;
+        this.turnos = turnos;
     }
 
     public Paciente() {
